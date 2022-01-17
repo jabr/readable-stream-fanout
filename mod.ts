123bc1718b4828f75a01229059cd6be74a87d7a2
Justin Bradford <jabradford@gmail.com>
2022-01-16 22:13:44 -0600

    Move the example/debug code to a separate (wip) test file

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
-import { Deferred, deferred, delay }
-    from "https://deno.land/std/async/mod.ts"
+import { Deferred, deferred } from "https://deno.land/std/async/mod.ts"
 
-let managerCount = 0
 type SourceWithBackpressure = { backpressure: Deferred<void> }
 type RSDC = ReadableStreamDefaultController
+
+let managerCount = 0
 class ReadableStreamManager {
     private source: SourceWithBackpressure
     public readable: ReadableStream
@@ -80,51 +80,4 @@ export default class ReadableStreamFanout {
             if (applyBackpressure) this.backpressure = deferred<void>()
         }
     }
-}
-
-if (import.meta.main) {
-    async function log(
-        name: string,
-        readable: ReadableStream,
-        count: number = Infinity
-    ) {
-        const reader = readable.getReader()
-        while (true) {
-            if (count-- <= 0) reader.cancel(`${name} lost interest`)
-            let { value, done } = await reader.read()
-            if (done) break
-            console.log(`${name} read`, value)
-        }
-        reader.releaseLock()
-        console.log(`${name} reading done`)
-    }
-
-    console.log('main')
-    const ts = new TransformStream
-    const wr = ts.writable.getWriter()
-    const fo = new ReadableStreamFanout(ts.readable)
-    fo.start()
-
-    let r1 = fo.add()
-    log('r1', r1)
-    let r2 = fo.add()
-    log('r2', r2)
-    let r3 = fo.add()
-    log('r3', r3, 2)
-    console.log('fanout branches', fo.size)
-
-    await wr.write({a: 1})
-
-    fo.close(r1)
-    console.log('fanout branches', fo.size)
-
-    await wr.write({a: 2})
-    console.log('fanout branches', fo.size)
-
-    await wr.write({a: 3})
-    console.log('fanout branches', fo.size)
-
-    await delay(2_000)
-    await wr.write({a: 4})
-    await delay(2_000)
 }
